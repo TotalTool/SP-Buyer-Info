9af7bc7c7f7cf023cb8e521fc5555485cb638fee
TOTALTOOL\josh.schubert <josh.schubert@totaltool.com>
2016-12-13 11:52:56 -0600

    Initial Commit

---
+++ Update-SPBuyerInfo.ps1
@@ -0,0 +1,38 @@
+﻿# Setup the correct modules for SharePoint Manipulation 
+if ( (Get-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null ) 
+{     
+   Add-PsSnapin Microsoft.SharePoint.PowerShell 
+} 
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Load CSV
+$delimeter =','
+$csvdata = Import-Csv -Path C:\Scripts\Buyers.csv -Delimiter $delimeter
+
+#Site
+$webURL = "http://sharepoint.totaltool.int/sales"
+#List
+$listname ="Stocking Vendors"
+
+#Sharepoint Column Names
+$buyer = "Buyer"
+$buyerInfo = "BuyerInfo"
+
+#Get Sharepoint site
+$web = Get-SPWeb $webURL
+#Get List and Items
+$list = $web.Lists[$listname]
+$items = $list.Items
+
+foreach ($line in $csvdata){
+    foreach($item in $items){
+        if($item[$buyer] -eq $line.'Buyer'){
+            $item[$buyerInfo] = $line.BuyerInfo
+            $item.Update();
+        }
+    }
+}
+$list.update()
+
+
+
